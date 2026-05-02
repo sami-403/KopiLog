e7e9b8c1f7ffe91c330186324e2d309bb85b75d7
Francisco Samily <francisco.samily1@gmail.com>
2026-05-02 20:56:03 -0300

    refactor: modifica o parametro da funcao resposavel por entregar a data atual
    Refatora módulo para se adequar a nova lógica do Módulo de formatação de datas.

--- utils/Descontos.js
+++ utils/Descontos.js
@@ -1,11 +1,11 @@
-import { dataFormatada } from "./Data.js";
+import { dataAtualFormatada } from "./Data.js";
 
 export function aplicarDesconto(valor, clienteAtual) {
   const descontoFidelidade = 0.05;
   const descontoAniversario = 0.1;
   let descontoTotal = 0;
 
-  if (clienteAtual.nascimento === dataFormatada()) {
+  if (clienteAtual.nascimento === dataAtualFormatada()) {
     descontoTotal += descontoAniversario;
   }
 
