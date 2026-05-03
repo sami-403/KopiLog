e9850637965a2111a449cb642fb21117d3bce2f4
Francisco Everton Maciel Rodrigues <macielyu@gmail.com>
2026-05-03 16:07:08 -0300

    feat/refactor: Acrescenta renderização dos botões de ações e refatora para melhor reutilização do código

--- view/clienteView.js
+++ view/clienteView.js
@@ -2,6 +2,21 @@ import { calcularDesconto } from "../utils/Descontos.js";
 import { formatarPercentual } from "../utils/Descontos.js";
 
 export class ClienteView {
+    #montarRow(cliente, key) {
+        return `
+            <td>${key}</td>
+            <td>${cliente.nome}</td>
+            <td>${cliente.nascimento}</td>
+            <td>${cliente.qntVisitou}</td>
+            <td>${calcularDesconto(cliente, true)}%</td>
+            <td>
+                <button type="button" data-action="visita" data-key="${key}">+ Visita</button>
+                <button type="button" data-action="editar" data-key="${key}">Editar</button>
+                <button type="button" data-action="excluir" data-key="${key}">Excluir</button>
+            </td>
+        `;
+    }
+
     render(clientes) {
         const clienteTable = document.getElementById('cliente-table');
         const tbody = clienteTable.querySelector('tbody');
@@ -12,13 +27,7 @@ export class ClienteView {
             const row = document.createElement('tr');
             row.dataset.key = key;
             row.id = `cliente-${key}`;
-            row.innerHTML = `
-                <td>${key}</td>
-                <td>${cliente.nome}</td>
-                <td>${cliente.nascimento}</td>
-                <td>${cliente.qntVisitou}</td>
-                <td>${calcularDesconto(cliente, true)}%</td>
-                `;
+            row.innerHTML = this.#montarRow(cliente, key);
             tbody.appendChild(row);
         });
     }
@@ -26,13 +35,7 @@ export class ClienteView {
     update(cliente, key) {
         const row = document.getElementById(`cliente-${key}`);
         if (row) {
-            row.innerHTML = `
-                <td>${key}</td>
-                <td>${cliente.nome}</td>
-                <td>${cliente.nascimento}</td>
-                <td>${cliente.qntVisitou}</td>
-                <td>${calcularDesconto(cliente, true)}%</td>
-                `;
+            row.innerHTML = this.#montarRow(cliente, key);
         } else {
             console.warn(`Cliente com chave ${key} não encontrado para atualização.`);
         }
